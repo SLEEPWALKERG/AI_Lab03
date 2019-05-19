52d9c84fd558e7a10360cf7fbc274c3660438038
SLEEPWALKERG <g1017557273@163.com>
2019-05-19 20:13:30 +0800

    5.19 pca

--- Main.py
+++ Main.py
@@ -1,18 +1,28 @@
 import os
 import cv2 as cv
 import numpy as np
-import PCA
-def Data_Init():
-    Base_Dir = os.path.abspath('.')
-    filename = Base_Dir + r"\att_faces\s1\1.pgm"
-    img = cv.imread(filename, 0)
-    rows, cols = img.shape
-    print(rows,cols)
-    #Vector_Img = np.zeros(1, rows*cols)
-    Vector_Img = np.reshape(img, (1,rows*cols))
-    print(len(Vector_Img[0]))
-    PCA.func(Vector_Img)
+from sklearn.decomposition import PCA
+import random
+
+
+def Data_Init(lst):
+    tmpdata = np.zeros(shape=(200, 10304))
+    base_Dir = os.path.abspath('.')
+    cnt = 0
+    for i in range(1, 41):
+        for each in lst:
+            filename = base_Dir + r"\att_faces\s" + str(i) + '\\' + str(each) + ".pgm"
+            img = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+            tmp_Img = np.reshape(img, (10304,))
+            tmpdata[cnt] = tmp_Img
+            cnt += 1
+    pca = PCA(n_components=40)
+    return pca.fit_transform(tmpdata)
 
 
 if __name__ == '__main__':
-    Data_Init()
+    lst = []
+    for i in range(5):
+        lst.append(random.randint(1, 10))
+    data = Data_Init(lst)
+    print(data)
--- PCA.py
+++
@@ -1,8 +0,0 @@
-from sklearn.decomposition import PCA
-import numpy as np
-import os
-
-def func(X):
-    pca = PCA(n_components=40)
-    pca.fit(X)
-    print(pca.explained_variance_ratio_)
